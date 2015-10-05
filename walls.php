99b134e91e9375ca51dc0e0f59f7269ad9f07057
Nickolas Nikolic <subclass@gmail.com>
2015-10-05 12:46:48 -0500

    styling

--- walls.php
+++ walls.php
@@ -29,7 +29,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/underscore-min.js"></script>
-    <script src="js/outside.js"></script>
+    <script src="js/walls.js"></script>
 </head>
 <body>
 <header>
