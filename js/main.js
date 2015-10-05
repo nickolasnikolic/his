ad516f4a14a744de56a82e99132d04f385490e42
Nickolas Nikolic <subclass@gmail.com>
2015-10-04 19:47:12 -0500

    items

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var requestList = ['B011JEUHM6','B013P16C8E','B00P03AKC0','B00YA15IH0','B00KACKORM','1612194443'];
     var responseList = [];
 
-    _.each(requestList,function(){
+    _.each(requestList,function(asin){
         $.getJSON('./api/amazon/lookup/' + asin)
             .success(function (data) {
                 responseList.push(data.Items.Item);
